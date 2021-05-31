7188254031c38386eb81644863acdc572bcbe367
JACK TRIMBOLI <Jack.Trimboli@yahoo.com>
2021-05-31 02:21:53 -0400

    configured tailwind

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import navbar from "./navbar";
+
+function Main(props) {
+  return (
+    <div>
+      <navbar />
+    </div>
+  );
+}
+export default Main;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function navbar(props) {
+  return <div className="nav">Navbar test</div>;
+}
+
+export default navbar;
