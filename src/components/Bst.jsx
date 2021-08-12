6b237108a9b2299a41ffacbf3fc91e32ffeaaea5
JACK TRIMBOLI <Jack.Trimboli@yahoo.com>
2021-08-12 15:45:47 -0400

    Bst node progress

--- src/components/Bst.jsx
+++ src/components/Bst.jsx
@@ -6,34 +6,59 @@ const Bst = (props) => {
   const [numNodes, setNumNodes] = useState(0);
   const [root, setRoot] = useState(null);
 
-  useEffect(() => {
-    if (props.listData.length > numNodes) {
-      //Addition of a node detected
-      handleAddNode();
-    } else if (props.listData.length < numNodes) {
-      //Removal of a node detected
-      setNumNodes(numNodes - 1);
-      handleRemoveNode();
-    }
-  }, [props.listData]);
+  // useEffect(() => {
+  //   if (props.listData.length > numNodes) {
+  //     //Addition of a node detected
+  //     handleAddNode();
+  //   } else if (props.listData.length < numNodes) {
+  //     //Removal of a node detected
+  //     setNumNodes(numNodes - 1);
+  //     handleRemoveNode();
+  //   }
+  // }, [props.listData]);
 
-  function handleAddNode() {
-    if (numNodes === 0) {
-      console.log(props.listData[0]);
-      setRoot(<Tree label={<BstNode value={props.listData[0]} />} />);
-    } else {
-      return;
-    }
-    setNumNodes(numNodes + 1);
-  }
-  function handleRemoveNode() {
-    return;
-  }
+  // function handleAddNode() {
+  //   if (numNodes === 0) {
+  //     console.log(props.listData[0]);
+  //   } else {
+  //     return;
+  //   }
+  //   setNumNodes(numNodes + 1);
+  // }
+  // function handleRemoveNode() {
+  //   return;
+  // }
+
   return (
     <div className="m-0 absolute w-full top-1/3 flex justify-evenly">
-      {root}
+      <Tree
+        label={null}
+        lineWidth={"5px"}
+        lineColor={"black"}
+        lineBorderRadius={"10px"}
+      >
+        <BstNode
+          value={1}
+          leftChild={{
+            value: "1",
+            rightChild: null,
+            leftChild: null,
+          }}
+          rightChild={{
+            value: "1",
+            rightChild: null,
+            leftChild: null,
+          }}
+        />
+      </Tree>
     </div>
   );
 };
+/*
+  --- BST Node Function --- 
+    1.
+    2.
+    3.
+*/
 
 export default Bst;
--- src/components/BstNode.jsx
+++ src/components/BstNode.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { TreeNode } from "react-organizational-chart";
 
-const BstNode = (props) => {
-  return (
+const BstNode = ({ value, leftChild, rightChild }) => {
+  const nodeLabel = (
     <div className="-my-2 mx-6 text-center inline-block">
       <div className="relative text-center transform bg-blue-600 z-10 rounded-full h-20 w-20 border-2 border-black border-4 shadow-md hover:scale-150 cursor-pointer">
         <p className="absolute w-full top-1/4 flex justify-center text-2xl">
-          {props.value}
+          {value}
         </p>
       </div>
     </div>
+  );
+  return (
+    <TreeNode label={nodeLabel}>
+      {leftChild && <BstNode {...leftChild} />}
+      {rightChild && <BstNode {...rightChild} />}
+    </TreeNode>
   );
 };
 
